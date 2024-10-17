a0990e133359ab28f21b781200e0269053483c6d
OrlovBohdan <b.orlov@e-u.edu.ua>
2024-10-17 17:12:43 +0300

    1

---
+++ src/task2.rs
@@ -0,0 +1,29 @@
+#[test]
+
+/*
+
+fn main() {
+   /* Fill in the blanks to make it print:
+   Hello world, I am
+   Sunface!
+   */
+   __("hello world, ");
+   __("I am");
+   __("Sunface!");
+}
+*/
+
+fn main() {
+    /* Fill in the blanks to make it print:
+    Hello world, I am
+    Sunface!
+    */
+    println!("Hello world, ");
+    println!("I am");
+    println!("Sunface!");
+}
+
+
+/*
+
+*/
